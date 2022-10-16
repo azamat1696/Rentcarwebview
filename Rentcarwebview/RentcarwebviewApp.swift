5cc53b15fa172daa8fa6fff21a4913c015c2b77e
AZAMAT YADYGEROV <73704375+azamat1696@users.noreply.github.com>
2022-10-16 07:50:41 +0300

    Initial Commit

---
+++ Rentcarwebview/RentcarwebviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  RentcarwebviewApp.swift
+//  Rentcarwebview
+//
+//  Created by Admin on 16.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct RentcarwebviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
